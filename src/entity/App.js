100724976c38f74f2b1dfa1fb179d6486c964699
sliu <ememebr.au@gmail.com>
2017-07-15 11:02:46 +1000

    lambda style

---
+++ src/entity/App.js
@@ -0,0 +1,3 @@
+/**
+ * Created by sliu on 12/07/2017.
+ */
---
+++ test/IotCertificateTest.js
@@ -0,0 +1,22 @@
+var AWS =require('aws-sdk');
+AWS.config= new AWS.Config({
+    accessKeyId: 'AKIAJR3BJ3ATVX3NKCSA'
+    ,secretAccessKey: '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'
+    ,region: 'us-west-2'
+});
+
+console.log(AWS.config);
+
+var iot = new AWS.Iot();
+
+var params ={
+    setAsActive:true || false
+};
+
+iot.createKeysAndCertificate(params,(err, data)=>{
+    if(err){
+        console.log(err, err.stack)
+    }else {
+        console.log(data)
+    }
+});
